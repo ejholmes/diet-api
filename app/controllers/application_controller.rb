7be5b8c146fe76df5e4ac421743446e07a5c83a7
Eric J. Holmes <eric@ejholmes.net>
2013-03-19 12:44:59 -0700

    Run mini profiler in production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :miniprofiler
+
+private
+
+  def miniprofiler
+    Rack::MiniProfiler.authorize_request
+  end
+
 end
